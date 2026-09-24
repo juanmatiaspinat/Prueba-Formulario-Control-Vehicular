90edc3437e7515415f535cd27190564d9e2160d9
Juan Matias Pinat <juanmatiaspinat@gmail.com>
2026-09-24 12:01:49 -0300

    Se simplifico nombre de archivo exportado omitiendo la hora

--- app.js
+++ app.js
@@ -666,10 +666,8 @@ async function generarReportePDF() {
         const dia = String(ahora.getDate()).padStart(2, "0");
         const mes = String(ahora.getMonth() + 1).padStart(2, "0");
         const anio = ahora.getFullYear();
-        const horas = String(ahora.getHours()).padStart(2, "0");
-        const minutos = String(ahora.getMinutes()).padStart(2, "0");
 
-        const nombreArchivo = `Reporte_DEA_Inspeccion_${dia}-${mes}-${anio}_${horas}-${minutos}.pdf`;
+        const nombreArchivo = `Reporte_DEA_Inspeccion_${dia}-${mes}-${anio}.pdf`;
         doc.save(nombreArchivo);
     } catch (err) {
         console.error("Error al exportar reporte:", err);
